5d529d290b19ab1a68e8c9a2296a83807ecd0a50
James Harris <james@crypticorange.co.uk>
2020-05-10 15:19:58 +0100

    Add sidebar

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -3,7 +3,21 @@ import React, { Component } from 'react';
 class Sidebar extends Component {
     state = {  }
     render() { 
-        return ( <div className="sidebar"></div> );
+        return ( <div className="sidebar">
+            <div className="sidebar__header">
+            <img src="/images/diagram.svg"></img>
+            <h1>COVID-19 Dashboard</h1>
+            </div>
+
+            <div className="sidebar__footer">
+                <a href="https://github.com/Cryptic-Orange/covid-dashboard" target="_blank">
+                    <img src="/images/github.svg"></img>
+                </a>
+                <a href="https//linkedin.com">
+                    <img src="/images/linkedin.svg"></img>
+                </a>
+            </div>
+        </div> );
     }
 }
  
